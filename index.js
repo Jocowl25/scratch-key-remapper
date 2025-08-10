62ca217548e919b623ea756a388178eb8336016c
jocowl <jocowl25@gmail.com>
2025-08-10 12:07:14 -0400

    make input fields dynamic

--- index.js
+++ index.js
@@ -1,17 +1,120 @@
 const keyChangeArray=[
-    {oldkey:"i",newkey:"a"}
+    {oldkey:"w",newkey:"u"},
+    {oldkey:"a",newkey:"g"},
+    {oldkey:"s",newkey:"h"},
+    {oldkey:"d",newkey:"j"},
 ]
-/*TO DO:
--use array to store key pairs in above format (turn into class?)
--dynamically add set of html fields that can set these pairs (splice array when erasing element)
-*/
+const exceptions=[
+    "up arrow","left arrow","right arrow","down arrow","space","any"
+]
+const fieldArray=[
+`
+    <div>
+            <label> Old key:
+            <input class="inputText oldkey" value="w" type="text" required  />
+            </label>
+            <label> New key:
+            <input class="inputText newkey" value="u" type="text" required />
+            </label>
+    </div>
+`,`
+    <div>
+            <label> Old key:
+            <input class="inputText oldkey" value="a" type="text" required  />
+            </label>
+            <label> New key:
+            <input class="inputText newkey" value="g" type="text" required  />
+            </label>
+        </div>
+`,`
+        <div>
+            <label> Old key:
+            <input class="inputText oldkey" value="s" type="text" required  />
+            </label>
+            <label> New key:
+            <input class="inputText newkey" value="h" type="text" required />
+            </label>
+        </div>
+`,`
+    <div>
+            <label> Old key:
+            <input class="inputText oldkey" value="d" type="text" required />
+            </label>
+            <label> New key:
+            <input class="inputText newkey" value="j" type="text" required />
+            </label>
+        </div>
+`
+]
+const inputSet=`<div>
+            <label> Old key:
+            <input class="inputText oldkey" type="text" required />
+            </label>
+            <label> New key:
+            <input class="inputText newkey" type="text" required />
+            </label>
+        </div>`
+
 let input
+const uploadText=document.querySelector(".uploadText")
+const addButton=document.querySelector(".add")
+const removeButton=document.querySelector(".remove")
+const keyfield=document.querySelector(".keyfield")
+keyfield.innerHTML=fieldArray.join("")
 const upload=document.querySelector(".upload")
-const go=document.querySelector(".go")
+const download=document.querySelector(".download")
+download.disabled=true
+const statusText=document.querySelector(".status")
+
+setInputs()
+
+function change(ele,i){
+if(ele.value.length==1||exceptions.includes(ele.value)){
+    if(ele.classList.contains("oldkey")){
+        keyChangeArray[i].oldkey=ele.value
+    }else{
+        keyChangeArray[i].newkey=ele.value
+    }
+}else{
+    if(ele.classList.contains("oldkey")){
+        ele.value=keyChangeArray[i].oldkey
+    }else{
+        ele.value=keyChangeArray[i].newkey
+    }
+}
+console.log(keyChangeArray)
+}
+
+addButton.addEventListener("click",()=>{
+    fieldArray.push(inputSet)
+    keyChangeArray.push({oldkey:"",newkey:""})
+    keyfield.innerHTML+=inputSet
+    setInputs()
+})
+removeButton.addEventListener("click",()=>{
+    if(fieldArray.length>1){
+        fieldArray.pop(inputSet)
+        keyChangeArray.pop({oldkey:"",newkey:""})
+        keyfield.innerHTML=fieldArray.join("")
+        setInputs()
+    }
+})
+
+function setInputs(){
+    document.querySelectorAll(".oldkey").forEach((ele,i)=>{
+        ele.value=keyChangeArray[i].oldkey
+        ele.addEventListener("change",()=>change(ele,i))
+    })  
+    document.querySelectorAll(".newkey").forEach((ele,i)=>{
+        ele.value=keyChangeArray[i].newkey
+        ele.addEventListener("change",()=>change(ele,i))
+    })
+}
+
 upload.addEventListener("click",()=>{
     inputElement.click()
 })
-go.addEventListener("click",()=>{
+download.addEventListener("click",()=>{
     try{
         let file=new File([JSON.stringify(convert(input))],"project.json",{type: 'json'})
         const link = document.createElement('a')
@@ -22,9 +125,11 @@ go.addEventListener("click",()=>{
         link.click()
         document.body.removeChild(link)
         window.URL.revokeObjectURL(url)
+        statusText.innerHTML="Done!"
         }
     catch(error){
         alert(`Something went wrong. ${error}`)
+        statusText.innerHTML=`Something went wrong. ${error}`
         }
 })
 const inputElement = document.getElementById("input");
@@ -35,7 +140,8 @@ inputElement.addEventListener("change", (e)=>{
     reader.addEventListener("load",() => {
         try{
             input= JSON.parse(reader.result);
-            go.style.display="block"
+            download.disabled=false
+            uploadText.innerHTML=`Uploaded ${file.name}`
         }catch(error){
             alert("This isn't a JSON file! Please upload a different file.")
         }
@@ -45,6 +151,7 @@ inputElement.addEventListener("change", (e)=>{
 }, false);
 
 function convert(input){
+        statusText.innerHTML="Converting..."
     let output=JSON.parse(JSON.stringify(input))
     input.targets.forEach((target,i)=>{
         let blocks=Object.keys(target.blocks)
@@ -59,5 +166,6 @@ function convert(input){
             }
         })
     })
+    statusText.innerHTML="Downloading..."
   return output
 }
